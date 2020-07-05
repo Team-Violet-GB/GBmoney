c387ccd516a4072eabf326a0798f34bf0f056d5d
ShinkarenkoMaksim <makc.pst@gmail.com>
2020-07-05 16:57:47 +0300

    Исправления по замечаниям

--- resources/js/views/User.vue
+++ resources/js/views/User.vue
@@ -2,36 +2,26 @@
 
 <template>
     <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="200px" class="cstm-ruleForm">
-        <el-form-item label="Ваш никнейм" prop="name">
-            <el-input v-model="ruleForm.name"></el-input>
-        </el-form-item>
         <el-form-item label="Ваш e-mail" prop="email">
-            <el-input v-model="ruleForm.email"></el-input>
+            <el-input v-model="ruleForm.email" id="email"></el-input>
         </el-form-item>
         <el-form-item label="Ваш пароль" prop="pass">
-            <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+            <el-input type="password" v-model="ruleForm.pass" autocomplete="off" show-password id="pass"></el-input>
+        </el-form-item>
+        <el-form-item label="Новый пароль" prop="newpass">
+            <el-input type="password" v-model="ruleForm.newpass" autocomplete="off" show-password id="newpass"></el-input>
         </el-form-item>
         <el-form-item label="Подтверждение пароля" prop="checkPass">
-            <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+            <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off" show-password id="checkPass"></el-input>
         </el-form-item>
         <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm')">Submit</el-button>
-            <el-button @click="resetForm('ruleForm')">Reset</el-button>
+            <el-button type="primary" @click="submitForm('ruleForm')">Изменить</el-button>
         </el-form-item>
     </el-form>
 </template>
 <script>
     export default {
         data() {
-            var checkName = (rule, value, callback) => {
-                if (!value) {
-                    return callback(new Error('Введите Ваш никнейм, пожалуйста'));
-                }
-                setTimeout(() => {
-                    //TODO реализовать проверку имени на дубликат из БД (callback())
-                    callback();
-                }, 1000);
-            };
             var checkEmail = (rule, value, callback) => {
                 if (value === '') {
                     callback(new Error('Введите Ваш e-mail, пожалуйста'));
@@ -54,10 +44,21 @@
                     callback();
                 }
             };
+            var validateNewPass = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('Введите новый пароль, пожалуйста'));
+                } else {
+                    if (this.ruleForm.checkPass !== '') {
+                        this.$refs.ruleForm.validateField('checkPass');
+                    }
+                    //TODO реализовать проверку корректности пароля в бэке (callback())
+                    callback();
+                }
+            };
             var validatePass2 = (rule, value, callback) => {
                 if (value === '') {
                     callback(new Error('Введите пароль повторно, пожалуйста'));
-                } else if (value !== this.ruleForm.pass) {
+                } else if (value !== this.ruleForm.newpass) {
                     callback(new Error('Пароли не совпадают!'));
                 } else {
                     callback();
@@ -66,24 +67,24 @@
             return {
                 ruleForm: {
                     pass: '',
+                    newpass: '',
                     checkPass: '',
-                    name: '',
-                    email: ''
+                    email: 'money@gb.ru' //TODO вставка имейла текущего пользователя
                 },
                 rules: {
                     pass: [
                         { validator: validatePass, trigger: 'blur' }
                     ],
+                    newpass: [
+                        { validator: validateNewPass, trigger: 'blur' }
+                    ],
                     checkPass: [
                         { validator: validatePass2, trigger: 'blur' }
                     ],
-                    name: [
-                        { validator: checkName, trigger: 'blur' }
-                    ],
                     email: [
-                        {validator: checkEmail, trigger: 'blur'}
+                        { validator: checkEmail, trigger: 'blur' }
                     ]
-                }
+                },
             };
         },
         methods: {
@@ -96,9 +97,6 @@
                         return false;
                     }
                 });
-            },
-            resetForm(formName) {
-                this.$refs[formName].resetFields();
             }
         }
     }
@@ -115,7 +113,12 @@
                 color: rgb(255, 208, 75);
             }
         }
+        input {
+            background-color: #4b4c55;
+            color: white;
+            &:hover, &:focus {
+                color: rgb(255, 208, 75);
+            }
+        }
     }
-
-
 </style>
