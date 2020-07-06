d306d15057dda375cf7cab51978fc9e63ec49059
Keymakh <ballena-espuma@yandex.ru>
2020-07-07 00:16:54 +0300

    init

--- public/js/app.js
+++ public/js/app.js
@@ -16063,8 +16063,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\OSPanel\domains\gbmoney.loc\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\OSPanel\domains\gbmoney.loc\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\OpenServer\domains\gbmoney.loc\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\OpenServer\domains\gbmoney.loc\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
