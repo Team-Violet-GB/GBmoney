dfb60c9a06a34352ad6afcf8d303f68223dee9b3
efrony <efron.vit@gmail.com>
2020-07-08 16:19:54 +0300

    ...

--- resources/js/components/homepage/CreateTransaction.vue
+++ resources/js/components/homepage/CreateTransaction.vue
@@ -212,6 +212,14 @@ import Calendar from "../homepage/Calendar"
   border-color: #ffffff;
 }
 
+input[type='number'] {
+    -moz-appearance:textfield;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+}
 
 </style>
 
