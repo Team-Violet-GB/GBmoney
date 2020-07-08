20cca77d338227d4bd884e9d7e44039a517b9175
rusla <suhrugen@gmail.com>
2020-07-08 18:42:09 +0700

    конец разметки, начало связи дропбоксов с поинтами

--- resources/js/views/Feed.vue
+++ resources/js/views/Feed.vue
@@ -7,30 +7,34 @@
             effect="dark">
         </el-alert>
 
+        <!--        заголовок группы транзакций-->
         <el-card v-if="!dataError" v-for="(item, index) in transactionData" :key="index" class="box-card">
-            <div slot="header" class="clearfix orange">
-                <h2>{{ item[0].date }}</h2>
-            </div>
+            <div slot="header" class="clearfix group-header">{{ dateLocal(item[0].date) }}</div>
+
+            <!--            список транзакций в группе-->
             <el-card v-for="(transaction, indexx) in item" :key="indexx" class="box-card">
                 <el-row type="flex" align="middle">
                     <el-col :span="18">
-                        <el-row :gutter="20">
-                            <el-col :span="4"><h2>{{ transaction.source }}</h2></el-col>
-                            <el-col :span="4"><h2>{{ transaction.amount }}</h2></el-col>
-                            <el-col :span="4"><h2>{{ transaction.receiver }}</h2></el-col>
+                        <el-row :gutter="20" class="tr-data-row">
+                            <el-col :span="4">{{ transaction.source }}</el-col>
+                            <el-col :span="4">{{ transaction.amount }} {{ rub }}</el-col>
+                            <el-col :span="4">{{ transaction.receiver }}</el-col>
                         </el-row>
                         <el-row>
-                            <el-col :span="24">
-                                <div class="grid-content bg-purple">{{ transaction.comment }}</div>
+                            <el-col :span="24" style="color: #8f95a7">
+                                {{ transaction.comment }}
                             </el-col>
                         </el-row>
                     </el-col>
+
+<!--                    кнопачки операций над транзакцией-->
                     <el-col :span="6">
                         <el-row type="flex" class="row-bg" justify="end">
                             <el-button-group>
                                 <el-button v-if="showInput && transaction.id == currentItem.id" type="success"
                                            @click="onSubmitData" size="small" icon="el-icon-check"></el-button>
-                                <el-button @click="showEditForm(transaction.id, index, indexx)" size="small" type="primary"
+                                <el-button @click="showEditForm(transaction.id, index, indexx)" size="small"
+                                           type="primary"
                                            icon="el-icon-edit"></el-button>
                                 <el-button @click="deleteTransaction(transaction.id)" size="small" type="danger"
                                            icon="el-icon-delete"></el-button>
@@ -38,65 +42,50 @@
                         </el-row>
                     </el-col>
                 </el-row>
+
+                <!--                выпадающий редактор транзакции-->
                 <el-collapse-transition>
                     <div v-if="transaction.id == currentItem.id" v-show="showInput" class="edit-view">
-                        <el-form ref="form" :model="editFormData" label-width="120px"
-                                 size="mini">
+                        <el-form ref="form" :model="editFormData" label-width=" 90px" size="small">
                             <el-row>
                                 <el-col :span="6">
-                                    <el-form-item label="Момент">
-                                        <el-col :span="11">
-                                            <el-date-picker
-                                                v-model="editFormData.date"
-                                                type="datetime"
-                                                placeholder="Когда?"
-                                                default-time="12:00:00">
-                                            </el-date-picker>
-                                        </el-col>
-                                    </el-form-item>
-                                </el-col>
-                                <el-col :span="6">
-                                    <el-form-item label="Из">
+                                    <el-form-item label="Откуда">
                                         <el-select v-model="editFormData.source" placeholder="источник">
                                             <el-option label="Наличные" value="Наличные"></el-option>
                                             <el-option label="Карта" value="Карта"></el-option>
                                         </el-select>
                                     </el-form-item>
                                 </el-col>
-
                                 <el-col :span="6">
-                                    <el-form-item label="В">
+                                    <el-form-item :label="rub">
+                                        <el-input clearable v-model="transactionData[index][indexx].amount"
+                                                  style="font-size: 1em;"></el-input>
+                                    </el-form-item>
+                                </el-col>
+                                <el-col :span="6">
+                                    <el-form-item label="Куда">
                                         <el-select v-model="editFormData.receiver" placeholder="приемник">
                                             <el-option label="Авто" value="Авто"></el-option>
                                             <el-option label="Разное" value="Разное"></el-option>
                                         </el-select>
                                     </el-form-item>
                                 </el-col>
-
                                 <el-col :span="6">
-                                    <el-form-item label="Сумма">
-                                        <el-input v-model="editFormData.amount"></el-input>
+                                    <el-form-item label="Когда">
+                                        <el-date-picker type="date" v-model="transactionData[index][indexx].date"
+                                                        style="font-size: 1em; width: 100%;"></el-date-picker>
                                     </el-form-item>
                                 </el-col>
                             </el-row>
                             <el-row>
                                 <el-col :span="24">
-
-
                                     <el-form-item label="Коментарий">
-                                        <el-input v-model="editFormData.comment"></el-input>
+                                        <el-input v-model="transactionData[index][indexx].comment"
+                                                  style="font-size: 1em;"></el-input>
                                     </el-form-item>
                                 </el-col>
                             </el-row>
-
-                            <!--                            <el-form-item>-->
-                            <!--                                <el-button type="success" @click="onSubmitData" icon="el-icon-check"></el-button>-->
-                            <!--                            </el-form-item>-->
                         </el-form>
-                        <!--                        <el-input type="text" v-model="transactionData[index][indexx].name"></el-input>-->
-                        <!--                        <el-input type="text" v-model="transactionData[index][indexx].amount"></el-input>-->
-                        <!--                        <el-input type="date" v-model="transactionData[index][indexx].date"></el-input>-->
-                        <!--                        <el-input type="text" v-model="transactionData[index][indexx].comment"></el-input>-->
                     </div>
                 </el-collapse-transition>
             </el-card>
@@ -130,9 +119,33 @@
                 transactionData: {}
             };
         },
+        computed: {
+            rub() {
+                return String.fromCharCode(8381);
+            }
+        },
         methods: {
+            dateLocal(date) {
+                return new Date(date).toLocaleDateString();
+            },
             onSubmitData() {
+                this.$message({
+                    showClose: true,
+                    message: "axios.put('/api/update/${currentItem.Id}', this.transactionData[this.currentItem.index]",
+                    duration: 6000,
+                    type: 'success'
+                });
+
                 this.showInput = false;
+
+                // axios
+                //     .put(`/api/update/${this.currentItem}`, this.transactionData[this.currentItem.index])
+                //     .then(response => {
+                //         this.getTransaction();
+                //         // console.log(response)
+                //     })
+                //     .catch(error => console.log(error));
+
             },
             getTransaction() {
                 axios
@@ -157,27 +170,19 @@
                     });
             },
             showEditForm(id, index, indexx) {
-                console.log(id, '\n', index, '\n', indexx);
                 this.currentItem.id = id;
                 this.currentItem.index = index;
                 this.currentItem.indexx = indexx;
                 this.showInput = true;
-
-                // axios
-                //     .put(`/api/update/${this.currentItem}`, {
-                //         title: this.tr.title,
-                //         desc: this.tr.desc
-                //     })
-                //     .then(response => {
-                //         this.tr.title = ''
-                //         this.tr.desc = ''
-                //         this.edit = false
-                //         this.getPosts()
-                //         console.log(response)
-                //     })
-                //     .catch(error => console.log(error));
             },
             deleteTransaction(id) {
+                this.$message({
+                    showClose: true,
+                    message: "axios.delete('/api/delete/${currentItem.Id}'",
+                    duration: 6000,
+                    type: 'error'
+                });
+
                 axios
                     .delete(`/api/del/${id}`)
                     .then(response => console.log(response))
@@ -185,6 +190,7 @@
                 this.getTransaction()
             }
         },
+        filters: {},
         mounted() {
             this.getTransaction()
         }
@@ -193,21 +199,25 @@
 
 <style scoped>
     .el-card {
-        margin-bottom: 30px;
-        cursor: default !important;
+        margin-top: 15px;
     }
 
-    .el-input {
-        font-size: 18pt;
-        cursor: pointer !important;
+    .group-header {
+        color: #0097FF;
+        font-size: xx-large;
+        font-weight: 500;
+        padding-left: 20px;
     }
 
-    .orange {
-        color: darkorange;
+    .tr-data-row {
+        font-size: x-large;
+        margin-bottom: 10px;
     }
 
     .edit-view {
-        padding-top: 20px;
+        margin-top: 30px;
+        padding: 20px 20px 0;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
     }
 
 </style>
