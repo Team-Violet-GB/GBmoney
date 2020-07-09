79fbeb39f19261af4b096454da24ccca11aa2e50
EvgeniyTuinov <47924160+EvgeniyTuinov@users.noreply.github.com>
2020-07-09 14:11:41 +0300

    test commit

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/registration', 'RegistrationController@create_account')->middleware('check.header');
+
