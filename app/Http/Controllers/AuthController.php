d9b766bead9d1d98bcada8d6b7ab47e8dca8847f
EvgeniyTuinov <47924160+EvgeniyTuinov@users.noreply.github.com>
2020-07-10 18:36:01 +0300

    удалил поле name

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,13 +19,13 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            //'name' => ['required', 'string', 'max:255'],
+            
             'email' => ['required', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'min:6'],
         ]);
 
         $user = User::create([
-           // 'name' => $request->name,
+
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
