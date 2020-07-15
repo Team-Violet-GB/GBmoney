047ffade270581ab1b699b976b3ef3acf6fc72c4
efrony <efron.vit@gmail.com>
2020-07-16 02:24:47 +0300

    home page Ajax

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 import user from './modules/user'
 import points from './modules/points'
 import transaction from './modules/transaction'
 import auth from './modules/auth'
 
 Vue.use(Vuex)
-
 
 export default new Vuex.Store({
     modules: {
