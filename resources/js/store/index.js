f0e1df22686553131089e88bc5e3d52fb2287343
rusla <suhrugen@gmail.com>
2020-07-18 19:40:50 +0700

    feed c авторизацией

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -4,6 +4,8 @@ import Vuex from 'vuex'
 import user from './modules/user'
 import points from './modules/points'
 import transaction from './modules/transaction'
+import feed from './modules/feed'
+import auth from './modules/auth'
 
 Vue.use(Vuex)
 
