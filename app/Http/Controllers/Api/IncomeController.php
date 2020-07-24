66d3a9d3b983f4fb11c4fe1eee71732a48367096
boganandrey <boganandrey@gmail.com>
2020-07-24 13:52:44 +0400

    to realize a create and update incomes by ajax request

--- app/Http/Controllers/Api/IncomeController.php
+++ app/Http/Controllers/Api/IncomeController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\IncomeFormRequest;
 use App\Http\Resources\IncomesCollection;
 use App\Models\Income;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -18,33 +20,50 @@ class IncomeController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return JsonResponse
      */
     public function index()
     {
-        //
+        /** @var Income $incomes */
+        $incomes = Income::query()->where('user_id', Auth::id())->get();
+
+        return response()->json(['data' => $incomes]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
-     * @return Response
+     * @param IncomeFormRequest $request
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(IncomeFormRequest $request)
     {
-        //
+        // Создаем новый объект доходов.
+        $income = new Income();
+
+        // Заполняем объект
+        $income->user_id = Auth::id();
+        $income->name = $request->name;
+        $income->icon_id = $request->icon_id;
+
+        // Сохраняем новый объект доходов.
+        $income->save();
+
+        return response()->json(['data' => $income], 200);
     }
 
     /**
      * Display the specified resource.
      *
      * @param int $id
-     * @return Response
+     * @return JsonResponse
      */
     public function show($id)
     {
-        //
+        /** @var Income $incomes */
+        $income = Income::query()->where('user_id', Auth::id())->find($id);
+
+        return response()->json(['data' => $income]);
     }
 
     /**
@@ -63,11 +82,18 @@ class IncomeController extends Controller
      * Remove the specified resource from storage.
      *
      * @param int $id
-     * @return Response
+     * @return void
      */
     public function destroy($id)
     {
-        //
+        /** @var Income $income */
+        $income = Income::query()->find($id);
+
+        // Удаляем объект.
+        $income->deleted = true;
+
+        // Сохраняем изменения.
+        $income->save();
     }
 
     /**
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -11,10 +11,9 @@ use Illuminate\Support\Facades\Auth;
  * @property-read int id
  * @property int user_id
  * @property int type
- * @property int amount
- * @property int date
- * @property int time
- * @property int comment
+ * @property float amount
+ * @property string date
+ * @property string comment
  * @property int wallet_id_from
  * @property int wallet_id_to
  * @property int income_id
