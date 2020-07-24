27854d6d398d316e748f316e39ae7016cd3d05af
boganandrey <boganandrey@gmail.com>
2020-07-24 19:37:39 +0400

    To realize api crud tags by ajax query.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,6 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Tag
+ * @property-read int id
+ * @property int user_id
+ * @property string name
+ * @property float expense_id
+ * @property boolean deleted
+ * @package App\Models
+ */
 class Tag extends Model
 {
     //
