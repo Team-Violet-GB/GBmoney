0498c10ebbcf70e2af24976c99637daf1cce0415
ShinkarenkoMaksim <makc.pst@gmail.com>
2020-08-05 11:29:19 +0300

    init2

--- resources/js/components/history/History.vue
+++ resources/js/components/history/History.vue
@@ -1,10 +1,26 @@
 <template>
-
+    <div class="container">
+        <line-chart
+            v-if="loaded"
+            :chartdata="chartdata"
+            :options="options"
+        />
+    </div>
 </template>
 
 <script>
+import LineChart from "./HistoryChart";
+import {mapActions, mapGetters, mapMutations} from 'vuex'
+
 export default {
-    name: "History"
+    name: "History",
+    components: { LineChart },
+    data: () => ({
+        loaded: false,
+        chartdata: null
+    }),
+    mounted() {
+    }
 }
 </script>
 
